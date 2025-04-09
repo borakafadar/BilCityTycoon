b1703b1944e612effc2b25516eff883232dde506
Zeynel Yıldırım <zeynel.yildirim@ug.bilkent.edu.tr>
2025-04-09 13:07:20 +0300

    Implemented Building, Decorations, Faculty classes.

--- core/src/main/java/io/github/bilcitytycoon/Decorations.java
+++ core/src/main/java/io/github/bilcitytycoon/Decorations.java
@@ -1,4 +1,11 @@
 package io.github.bilcitytycoon;
 
-public class Decorations implements Building{
+public class Decorations extends Building{
+
+    public Decorations(String name, double cost){
+        super(name,cost);
+    }
+    public void getInfo() {
+        //TODO
+    }
 }
--- core/src/main/java/io/github/bilcitytycoon/OtherBuildings.java
+++ core/src/main/java/io/github/bilcitytycoon/OtherBuildings.java
@@ -1,4 +1,6 @@
 package io.github.bilcitytycoon;
 
-public class OtherBuildings implements  Building{
+public class OtherBuildings extends Building{
+    public OtherBuildings(String name, double buildCost, double bill) {}
 }
+ 
