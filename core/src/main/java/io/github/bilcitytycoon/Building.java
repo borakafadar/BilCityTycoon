7e95d338247084f6b3d24e311188346c3eb72f0c
Zeynel Yıldırım <zeynel.yildirim@ug.bilkent.edu.tr>
2025-04-09 13:21:22 +0300

    added methods in Building, Faculty, OtherBuildings.

--- core/src/main/java/io/github/bilcitytycoon/Building.java
+++ core/src/main/java/io/github/bilcitytycoon/Building.java
@@ -3,12 +3,21 @@ package io.github.bilcitytycoon;
 public abstract class Building {
     protected String name;
     protected double buildCost;
-    protected double bill;
 
 
     public Building(String name, double cost){
         this.name = name;
         this.buildCost = cost;
     }
+
     public abstract void getInfo();
+
+    public String getName(){
+        return this.name;
+    }
+    
+    public double getCost(){
+        return this.buildCost;
+    }
+
 }
