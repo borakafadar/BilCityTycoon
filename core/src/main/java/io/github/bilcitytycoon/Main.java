f977ce035e9f17c2abe1a761115376e7fc858179
Yaşar Delibaş <yasardelibas33@gmail.com>
2025-04-09 09:46:05 +0300

    initial classes

--- core/src/main/java/io/github/bilcitytycoon/Main.java
+++ core/src/main/java/io/github/bilcitytycoon/Main.java
@@ -5,24 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private Viewport viewport;
+    private static final float WORLD_WIDTH = 1920;
+    private static final float WORLD_HEIGHT = 1080;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("bilkentDrone.jpg");
+        viewport = new ScreenViewport();
+        viewport.apply();
+
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
-        batch.draw(image, 140, 210);
+
+        batch.draw(image, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
         batch.end();
     }
 
