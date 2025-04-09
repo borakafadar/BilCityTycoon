fe1d1788f9a36b5df882617dcdb95b0d41bcb7b3
Bora Kafadar <borakafadar@gmail.com>
2025-04-09 10:26:17 +0300

    Implemented some of Player.java and added Map and Leaderboard classes

---
+++ core/src/main/java/io/github/bilcitytycoon/Leaderboard.java
@@ -0,0 +1,19 @@
+package io.github.bilcitytycoon;
+
+import java.util.ArrayList;
+
+public class Leaderboard {
+    private ArrayList<Player> players;
+
+    public Leaderboard() {
+        players = new ArrayList<Player>();
+        createOtherPlayers();
+    }
+
+    public void createOtherPlayers() {
+        //TODO:implement other players, 80-100 other universities
+
+    }
+
+
+}
