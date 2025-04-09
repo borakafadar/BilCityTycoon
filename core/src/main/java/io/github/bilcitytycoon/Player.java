db905c5506d284a921302eee44a3108975f14dac
Bora Kafadar <borakafadar@gmail.com>
2025-04-09 10:26:17 +0300

    Implemented some of Player.java and added Map and Leaderboard classes

--- core/src/main/java/io/github/bilcitytycoon/Player.java
+++ core/src/main/java/io/github/bilcitytycoon/Player.java
@@ -1,4 +1,31 @@
 package io.github.bilcitytycoon;
 
-public class Player {
+import java.util.ArrayList;
+
+public class Player implements Comparable<Player> {
+    private int coin;
+    private String name;
+    private int universityReputationPoint;
+    private int studentSatisfactionRate;
+    private ArrayList<Building> buildings;
+    private int leaderboardRanking;
+
+
+
+    public int getCoin() {
+        return coin;
+    }
+    public void setCoin(int newCoin) {
+        this.coin = newCoin;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public int compareTo(Player o) {
+        //TODO: comparable method to use for leaderboard ranking
+    }
+
 }
