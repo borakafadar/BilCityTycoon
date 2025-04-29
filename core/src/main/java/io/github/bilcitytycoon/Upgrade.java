b258a63da393326791964f4bc88138106825672d
Eylül Ündem <eylul.undem@ug.bilkent.edu.tr>
2025-04-29 21:13:58 +0300

    Added the foundations of Upgrade and MoneyHandler classes
    Added the variables and methods of Upgrade and MoneyHandler classes

---
+++ core/src/main/java/io/github/bilcitytycoon/Upgrade.java
@@ -0,0 +1,16 @@
+package io.github.bilcitytycoon;
+
+public class Upgrade {
+    private String name;
+    private int upgradeCost;
+    private int constructionTime;
+    private Building building;
+    private String info;
+    private boolean isMade;
+
+    public String
+
+    public void updateBudget(){
+        //TODO
+    }
+}
