fa14f66ba41b69f4b90bc9790c5b6c137f587f96
Eylül Ündem <eylul.undem@ug.bilkent.edu.tr>
2025-05-07 22:16:22 +0300

    Implemented the MoneyHandler class
    Implemented the MoneyHandler class with methods to update and calculate income, expenses and net income

--- core/src/main/java/io/github/bilcitytycoon/MoneyHandler.java
+++ core/src/main/java/io/github/bilcitytycoon/MoneyHandler.java
@@ -3,20 +3,39 @@ package io.github.bilcitytycoon;
 public class MoneyHandler {
     private int income;
     private int expense;
+    private int netIncome;
 
+    public MoneyHandler() {
+        this.income = 0;
+        this.expense = 0;
+        this.netIncome = 0;
+    }
     public int getIncome(){
         return this.income;
     }
 
-    public void updateIncome(){
-        //TODO
+    //Called when a new source of income emerges
+    public void updateIncome(int amount){
+        this.income += amount;
+        updateNetIncome();
     }
 
     public int getExpense(){
         return this.expense;
     }
 
-    public void updateExpenses(){
-        //TODO
+    //Called when a new source of expense emerges
+    public void updateExpense(int amount){
+        this.expense += amount;
+        updateNetIncome();
+    }
+
+    public int getNetIncome(){
+        return this.netIncome;
+    }
+
+    //Called when net income is needed to be calculated with either an updated income or expense
+    public void updateNetIncome(){
+        this.netIncome = income - expense;
     }
 }
