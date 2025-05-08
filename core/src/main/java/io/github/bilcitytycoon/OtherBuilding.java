13939d73f6e03a1cf5536ebd09df7dac242f4b6a
Yaşar Delibaş <yasar.delibas@ug.bilkent.edu.tr>
2025-05-08 14:39:37 +0300

    Updating OtherBuilding.java
    I have added an exception handler to the constructor while getting the image from the field. Also i override toString() and getInfo() methods

--- core/src/main/java/io/github/bilcitytycoon/OtherBuilding.java
+++ core/src/main/java/io/github/bilcitytycoon/OtherBuilding.java
@@ -14,27 +14,45 @@ public class OtherBuilding extends Building{
         super(name, cost, bill);
         this.income = income;
         this.buildTime = buildTime;
-        this.image = new Image(new Texture(Gdx.files.internal(imagePath)));
         this.info = info;
-    }
 
+        // Load the image for the building, with validation
+        if (imagePath == null || imagePath.isEmpty()) {
+            throw new IllegalArgumentException("Image path cannot be null or empty");
+        }
+
+        if (Gdx.files.internal(imagePath).exists()) {
+            this.image = new Image(new Texture(Gdx.files.internal(imagePath)));
+        } else {
+            throw new IllegalArgumentException("Image file not found: " + imagePath);
+        }
+    }
+    // Getter methods for the building's properties
     public double getIncome() {
         return this.income;
     }
 
     public String getInfo(){
-        //TODO
-        return null; //temp
+        return "Building Name: " + this.name + "\n" +
+               "Cost: " + this.buildCost + " BilCoins\n" +
+               "Monthly Bill: " + this.bill + " BilCoins\n" +
+               "Income: " + this.income + " BilCoins\n" +
+               "Build Time: " + this.buildTime + " days\n" +
+               "Info: " + this.info;
     }
     public Image getImage(){
-        return null;
-        //TODO temp
+        return this.image;
     }
     public int getBuildTime(){
         return this.buildTime;
     }
 
-
+    // Override toString for a detailed representation
+    @Override
+    public String toString() {
+        return "OtherBuilding{name='" + name + "', cost=" + buildCost + ", bill=" + bill +
+                ", income=" + income + ", buildTime=" + buildTime + "}";
+    }
 
 }
 
