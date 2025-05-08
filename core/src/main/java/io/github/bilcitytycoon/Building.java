97118668a94113013a3d304ed5586489bb278647
Yaşar Delibaş <yasar.delibas@ug.bilkent.edu.tr>
2025-05-08 14:41:49 +0300

    Adding some getters and implementing the constructor
    I have added the instance variable info and constructionTime. I also implemented getBill(), getConstructionTime(), setConstructionTime() and setInfo()

--- core/src/main/java/io/github/bilcitytycoon/Building.java
+++ core/src/main/java/io/github/bilcitytycoon/Building.java
@@ -1,25 +1,51 @@
 package io.github.bilcitytycoon;
 
+/*
+ * Abstract class representing a building in the game.
+ * It contains common properties and methods for all buildings. 
+ * Specific building types (e.g., Faculty, Rectorate) will extend this class.
+ */
 public abstract class Building {
     protected String name;
+    protected String info;
     protected int buildCost;
     protected int bill;
+    protected int constructionTime;
 
-
+    // Constructor for the Building class
+    // Initializes the building's name, cost, and bill.
     public Building(String name, int cost, int bill){
+        this.info = "No additional information";
+        this.constructionTime = 0;
         this.name = name;
         this.buildCost = cost;
         this.bill = bill;
     }
 
+    //An abstract method to get the building's information.
     public abstract String getInfo();
 
+    // Getter methods for the building's properties
     public String getName(){
         return this.name;
     }
 
     public int getCost(){
         return this.buildCost;
+    }
+
+    public int getBill(){
+        return this.bill;
+    }
+    public int getConstructionTime(){
+        return this.constructionTime;
+    }
+    //Setter methods for the building's properties
+    public void setConstructionTime(int time){
+        this.constructionTime = time;
+    }
+    public void setInfo(String info){
+        this.info = info;
     }
 
 }
